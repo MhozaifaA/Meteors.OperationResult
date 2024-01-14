8e273377d45289b245d19ac55a11ae7b26a8d926
MhozaifaA <botchey.mha@hotmail.com>
2024-01-14 15:06:49 +0400

    improve ToString allclocated

--- src/Enum/Statuses.cs
+++ src/Enum/Statuses.cs
@@ -161,14 +161,14 @@ namespace Meteors.OperationContext
         {
             return status switch
             {
-                _Statuses.UnKnown => "UnKnown",//nameof
-                _Statuses.Success => "Success",
-                _Statuses.Exist => "Exist",
-                _Statuses.NotExist => "NotExist",
-                _Statuses.Failed => "Failed",
-                _Statuses.Forbidden => "Forbidden",
-                _Statuses.Exception => "Exception",
-                _Statuses.Unauthorized => "Unauthorized",
+                _Statuses.UnKnown => nameof(_Statuses.UnKnown),//nameof
+                _Statuses.Success => nameof(_Statuses.Success),
+                _Statuses.Exist => nameof(_Statuses.Exist),
+                _Statuses.NotExist => nameof(_Statuses.NotExist),
+                _Statuses.Failed => nameof(_Statuses.Failed),
+                _Statuses.Forbidden => nameof(_Statuses.Forbidden),
+                _Statuses.Exception => nameof(_Statuses.Exception),
+                _Statuses.Unauthorized => nameof(_Statuses.Unauthorized),
                 _ => throw new System.NotImplementedException(),
             };
         }
@@ -176,32 +176,38 @@ namespace Meteors.OperationContext
         {
             return status switch
             {
-                { value: _Statuses.UnKnown } => "UnKnown",//nameof
-                { value: _Statuses.Success } => "Success",
-                { value: _Statuses.Exist } => "Exist",
-                { value: _Statuses.NotExist } => "NotExist",
-                { value: _Statuses.Failed } => "Failed",
-                { value: _Statuses.Forbidden } => "Forbidden",
-                { value: _Statuses.Exception } => "Exception",
-                { value: _Statuses.Unauthorized } => "Unauthorized",
+                { value: _Statuses.UnKnown } => nameof(_Statuses.UnKnown),
+                { value: _Statuses.Success } => nameof(_Statuses.Success),
+                { value: _Statuses.Exist } => nameof(_Statuses.Exist),
+                { value: _Statuses.NotExist } => nameof(_Statuses.NotExist),
+                { value: _Statuses.Failed } => nameof(_Statuses.Failed),
+                { value: _Statuses.Forbidden } => nameof(_Statuses.Forbidden),
+                { value: _Statuses.Exception } => nameof(_Statuses.Exception),
+                { value: _Statuses.Unauthorized } => nameof(_Statuses.Unauthorized),
                 _ => throw new System.NotImplementedException(),
             };
         }
 
         internal static Statuses ToPerEnumStatuses(this string? status)
         {
-            return status.ToUpper() switch
-            {
-                 null or "UNKNOWN"  => _Statuses.UnKnown,
-                "SUCCESS" => _Statuses.Success, 
-                "EXIST" => _Statuses.Exist,
-                "NOTEXIST" => _Statuses.NotExist,
-                "FAILED" => _Statuses.Failed,
-                "FORBIDDEN" => _Statuses.Forbidden,
-                "EXCEPTION" => _Statuses.Exception,
-                "UNAUTHORIZED" => _Statuses.Unauthorized,
-                _ => throw new System.NotImplementedException(),
-            };
+            if (status is null || status.Equals(nameof(_Statuses.UnKnown), StringComparison.OrdinalIgnoreCase))
+                return _Statuses.UnKnown;
+            if (status.Equals(nameof(_Statuses.Success), StringComparison.OrdinalIgnoreCase))
+                return _Statuses.Success;
+            if (status.Equals(nameof(_Statuses.Exist), StringComparison.OrdinalIgnoreCase))
+                return _Statuses.Exist;
+            if (status.Equals(nameof(_Statuses.NotExist), StringComparison.OrdinalIgnoreCase))
+                return _Statuses.NotExist;
+            if (status.Equals(nameof(_Statuses.Failed), StringComparison.OrdinalIgnoreCase))
+                return _Statuses.Failed;
+            if (status.Equals(nameof(_Statuses.Forbidden), StringComparison.OrdinalIgnoreCase))
+                return _Statuses.Forbidden;
+            if (status.Equals(nameof(_Statuses.Exception), StringComparison.OrdinalIgnoreCase))
+                return _Statuses.Exception;
+            if (status.Equals(nameof(_Statuses.Unauthorized), StringComparison.OrdinalIgnoreCase))
+                return _Statuses.Unauthorized;
+
+            throw new System.NotImplementedException($"{status} is not of {nameof(OperationResult)}/{nameof(Statuses)}");
         }
     }
 
@@ -210,7 +216,7 @@ namespace Meteors.OperationContext
     {
         public override Statuses? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             => reader.GetString().ToPerEnumStatuses();
-        
+
 
         public override void Write(Utf8JsonWriter writer, Statuses value, JsonSerializerOptions options)
             => writer.WriteStringValue(value.ToString());
